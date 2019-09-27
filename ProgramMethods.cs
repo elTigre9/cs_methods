1bb4d86e444f69ea668d5368d3b246d1cd05a6a7
elTigre9 <33560876+elTigre9@users.noreply.github.com>
2019-09-27 17:58:33 -0500

    Add files via upload

---
+++ ProgramMethods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MethodsHW
+{
+    class ProgramMethods
+    {
+        static void Main(string[] args)
+        {
+            var program = new ProgramMethods();
+            program.newMethod();
+            program.banana(9f, 3.3f);
+            program.banana(12f, 4f);
+
+            int[] luckyNums = { 4, 5, 6, 1, 2, 7, 9, 10 };
+            program.final(luckyNums);
+        }
+
+        public void newMethod()
+        {
+            Console.WriteLine("something");
+        }
+
+        public void banana(float buddy, float guy)
+        {
+            var avg = buddy + guy / 2;
+            Console.WriteLine(avg);
+        }
+
+        public void final(int[] param3)
+        {
+            foreach(int x in param3)
+            {
+                if(x == 7)
+                {
+                    Console.WriteLine("Lucky you!");
+                }
+            }
+        }
+    }
+}
